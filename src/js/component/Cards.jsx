1bf38fad20c9c923bfa2a692d8c633b5d796470f
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2023-10-11 03:03:16 +0000

    add_imagenes

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -4,7 +4,7 @@ const Cards = (props) => {
   return (
     
     <div class="card text-center">
-        <img src="https://picsum.photos/500/325" class="card-img-top" alt="Img Card" />
+        <img src={props.imagen} class="card-img-top" alt="Img Card" />
         <div class="card-body">
             <h5 class="card-title">{props.title}</h5>
             <p class="card-text">{props.description}</p>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,10 +7,10 @@ import Cards from "./Cards.jsx";
 //create your first component
 const Home = () => {
 	const cards = [
-		{title: "first-title", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In exercitationem sapiente inventore provident animi reprehenderit."},
-		{title: "second-title", description: "consectetur adipisicing elit. In exercitationem sapiente inventore provident animi reprehenderit Lorem ipsum dolor sit amet."}, 
-		{title: "third-title", description: "In exercitationem sapiente inventore provident animi reprehenderit, Lorem ipsum dolor sit amet, consectetur adipisicing elit."},
-		{title: "fourth-title", description: "ipsum dolor sit amet, consectetur adipisicing elit. In exercitationem Lorem sapiente inventore provident animi reprehenderit."}
+		{title: "first-title", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In exercitationem sapiente inventore provident animi reprehenderit.", imagen: "https://picsum.photos/500/325"},
+		{title: "second-title", description: "consectetur adipisicing elit. In exercitationem sapiente inventore provident animi reprehenderit Lorem ipsum dolor sit amet.", imagen: "https://picsum.photos/500/325"}, 
+		{title: "third-title", description: "In exercitationem sapiente inventore provident animi reprehenderit, Lorem ipsum dolor sit amet, consectetur adipisicing elit.", imagen: "https://picsum.photos/500/325"}, 
+		{title: "fourth-title", description: "ipsum dolor sit amet, consectetur adipisicing elit. In exercitationem Lorem sapiente inventore provident animi reprehenderit.", imagen: "https://picsum.photos/500/325"}
 	]
 	return (
 		<div>
@@ -20,7 +20,7 @@ const Home = () => {
 				<div class="cardStyle">
 					<div class="row row-cols-1 row-cols-md-4 g-4">
 						{cards.map((card,index) => {
-							return (<Cards title={card.title} description={card.description} key={index}/>)
+							return (<Cards title={card.title} description={card.description} imagen={card.imagen} key={index}/>)
 						})}
 					</div>
 				</div>
